728a51dfcb9e685f7d59bde8f20b6dce35611cb0
CG <congodsted@protonmail.com>
2022-07-22 21:50:58 -0400

    Bugfix - images on top of images breaking everything

--- script.js
+++ script.js
@@ -44,6 +44,7 @@ class Tier{
     }
 }
 let tierList = new TierList();
+//the default tiers, s-f
 tierList.addTier(new Tier("red","s"));
 tierList.addTier(new Tier("orange","a"));
 tierList.addTier(new Tier("#ffff00","b"));
@@ -99,27 +100,26 @@ function endDrag(e) {
     position.forEach(function(element) {
         if (element.tagName == "TD") { //find a part of the table to insert
             //If its a TD, we know it has a TR above it
-            let tempImage = document.querySelector("#dragged").style.backgroundImage
+            let tempImage = document.getElementById("dragged").style.backgroundImage
             tempImage = tempImage.slice(5, -2)
             let template = `<div draggable="true" class="potentialdrag" style="background-image: url(${tempImage});" ></div>`;
 
             element.innerHTML += template;
             document.getElementById("dragged").remove();
 
-        } else { //if isn't dragged into the table
-            //sometimes elements get stuck in a dragged state, fixes
-            if (document.getElementById('dragged')) {
-                document.getElementById("dragged").id = "";
-            }
         }
     });
     //moved elements lose their event listeners after being dragged
+    if (document.getElementById('dragged')) {
+        document.getElementById("dragged").id = "";
+    }
     addListeners()
 
 
 }
 
 function addNew() {
+    //opens the plus menu. this may end up being renamed.
     document.getElementById("addDrop").className = "dropshown";
     document.getElementById("addDrop").style.opacity = 1;
     document.getElementById("addDrop").innerHTML = `
@@ -148,11 +148,13 @@ function addNew() {
 
 function addSelection(select) {
     if (select == "upload") {
+        //Add Image from Upload
         document.getElementById("addDrop").innerHTML =
             `<div id="addimagediv">
             <label>Add new image: </label>
             <input multiple onchange="imageRead('file')" type="file" accept="image/*" id="fileselect">
         </div>`;
+        //Add Image from URL
     } else if (select == "url") {
         document.getElementById("addDrop").innerHTML =
             `<div id="addurldiv">
@@ -160,6 +162,7 @@ function addSelection(select) {
             <input type="url" name="urlselect" id="urlselect">
             <button onclick="imageRead(document.getElementById('urlselect').value)" id="addtierbutton">Add Image</button>
         </div>`;
+        //Add Text to Tier List
     } else if (select == "text") {
         document.getElementById("addDrop").innerHTML =
             `<div id="addtextdiv">
@@ -167,6 +170,7 @@ function addSelection(select) {
             <input type="text" name="textselect" id="textselect">
             <button onclick="addTier()" id="addtierbutton">Add Text</button>
         </div>`;
+        //Add New Tier
     } else if (select == "newtier") {
         document.getElementById("addDrop").innerHTML =
             `<div id="addtierdiv">
@@ -185,12 +189,14 @@ function addSelection(select) {
 }
 
 function closePlus() {
+    //hides away the plus menu. animations are a little busted but uh. woops.
     document.getElementById("addDrop").style.opacity = 0;
     document.getElementById("addDrop").className = "hidden";
 
 }
 
 function checkPlus() {
+    //check if user is hovering over the dropdown, or the plus button. closes if not.
     if (document.querySelector(".dropshown:hover") == null && document.querySelector("#new:hover") == null) {
         closePlus()
     }
@@ -198,6 +204,7 @@ function checkPlus() {
 
 }
 function addListeners() {
+    //adds the necessary event listeners for the Drag
     items = document.querySelectorAll('.potentialdrag');
     items.forEach(function(item) {
         item.addEventListener('dragstart', startDrag);
